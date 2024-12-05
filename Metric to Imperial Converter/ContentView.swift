a7b81f2064c4ef323d504224b4253f701cf5f571
Diana Dashynevich <ddashinevich@gmail.com>
2024-12-05 14:01:17 +0100

    Initial Commit

---
+++ Metric to Imperial Converter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Metric to Imperial Converter
+//
+//  Created by Diana Dashinevich on 05/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Metric to Imperial Converter/Metric_to_Imperial_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metric_to_Imperial_ConverterApp.swift
+//  Metric to Imperial Converter
+//
+//  Created by Diana Dashinevich on 05/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Metric_to_Imperial_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
